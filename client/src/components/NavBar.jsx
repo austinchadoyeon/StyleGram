e6928c0bfc5178807fa1b4ddbee5ec7086abd131
Austin Yeon <austinyeon@gmail.com>
2021-07-08 01:32:59 -0700

    added collapsing functionality to navigation bar and created connection file (gitignored) for database connection

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,16 +7,23 @@ export default function NavigationBar (props) {
   return (
       <Navbar bg='light' expand='xl'>
         <Navbar.Brand>Moda</Navbar.Brand>
-        <Nav>
-          <Nav.Link>Home</Nav.Link>
-          <Nav.Link onClick={props.postPic}>Post a Pic</Nav.Link>
-          <PostPic cancel={props.postPic} show={props.show}/>
-          <Nav.Link>Rate a Fit</Nav.Link>
-          <Nav.Link>Trending</Nav.Link>
-          <Nav.Link>Upgrade</Nav.Link>
-          <Nav.Link>Help</Nav.Link>
-          <Nav.Link onClick={props.handleLogout}>Logout</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id='navbarScroll'>
+          <Nav
+            className="mr-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link>Home</Nav.Link>
+            <Nav.Link onClick={props.postPic}>Post a Pic</Nav.Link>
+            <PostPic cancel={props.postPic} show={props.show}/>
+            <Nav.Link>Rate a Fit</Nav.Link>
+            <Nav.Link>Trending</Nav.Link>
+            <Nav.Link>Upgrade</Nav.Link>
+            <Nav.Link>Help</Nav.Link>
+            <Nav.Link onClick={props.handleLogout}>Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
   )
 }
